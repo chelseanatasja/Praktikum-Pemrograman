c5ea6859e65b1a1824e3f8de994248a10d5392d8
chelseanatasja <162960783+chelseanatasja@users.noreply.github.com>
2024-09-03 23:27:50 +0700

    Add files via upload

---
+++ quadratic eq.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    float a, b, c;
+
+    cout<<"a coefficient = ";
+    cin>>a;
+    cout<<"b coeffiicient = ";
+    cin>>b;
+    cout<<"c coefficient = ";
+    cin>>c;
+
+    float d;
+    d= b*b-4*a*c;
+
+    if(d>0){
+        float x1;
+        float x2;
+        x1 = (-b+sqrt(d))/2*a;
+        x2 = (-b-sqrt(d))/2*a;
+        cout<<"x1= "<<x1<<endl<<"x2= "<<x2<<endl;
+    }
+
+
+     if(d==0){
+        float x;
+        x = -b/(2*a);
+        cout<<"x = "<<x<<endl;
+    }
+
+     if(d<0){
+        cout<<"There are no real roots";
+    }
+
+
+    return 0;
+
+}
