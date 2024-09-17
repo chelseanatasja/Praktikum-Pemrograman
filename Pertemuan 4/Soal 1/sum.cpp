bd057286e9c2f4ec6a8d9c8e8a09ae46fb3d1f54
chelseanatasja <162960783+chelseanatasja@users.noreply.github.com>
2024-09-17 14:49:04 +0700

    Add files via upload

---
+++ Pertemuan 4/Soal 1/sum.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Input: ";
+    cin >> n;
+    int sum = 0;
+
+    for (int i = 1; i <= n; ++i) {
+        sum += i;
+    }
+    cout << "Output: Sum = " << sum;
+
+}
+
+
+   
