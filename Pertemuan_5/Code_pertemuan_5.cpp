d2d49d05fc84fabdcad895629bb0001034d5c9ec
chelseanatasja <162960783+chelseanatasja@users.noreply.github.com>
2024-10-10 07:16:02 +0000

    yaw

--- Pertemuan_5/Code_pertemuan_5.cpp
+++ Pertemuan_5/Code_pertemuan_5.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 
-int main(){
+std::vector<std::string> fizzBuzz() {
     std::vector<std::string> results;
 
     for(int i=1; i<=100; ++i){
@@ -15,8 +15,34 @@ int main(){
             results.push_back(std::to_string(i));
         }
     }
-    for (const auto& result : results){
-        std::cout << result << std::endl;
+    return result;
     }
+
+    std::vector<int> reverseArray(const std::vector<int>& arr) {
+    std::vector<int> reversedArr;
+    for (int i = arr.size() - 1; i >= 0; --i) {
+        reversedArr.push_back(arr[i]);
+    }
+    return reversedArr;
+}
+
+int main() {
+    std::vector<std::string> fizzBuzzResult = fizzBuzz();
+    
+    for (const std::string& str : fizzBuzzResult) {
+        std::cout << str << std::endl;
+    }
+
+    std::vector<int> arr = {1, 2, 3, 4, 5};
+    std::vector<int> reversedArr = reverseArray(arr);
+
+    std::cout << "Reversed array: ";
+    for (int num : reversedArr) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
     return 0;
 }
+    
+
